60a6afb0e9763f7517e44c0c10a4002f71a42ef4
Sabitha Radhakrishnan <saradhakrish@teksystems.com>
2021-11-05 06:59:21 -0500

    :construction:

--- src/main/java/coding/challenge/rest/CapitalCityRequest.java
+++ src/main/java/coding/challenge/rest/CapitalCityRequest.java
@@ -1,9 +1,21 @@
 package coding.challenge.rest;
 
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriBuilder;
+
 public class CapitalCityRequest {
+    private static String PATH = "https://restcountries.com/v3.1/name/";
+    private final Client client;
+    private final WebTarget target;
 
     private CapitalCityRequest(String countryInput) {
-
+        String finalPATH = PATH+countryInput;
+        client = ClientBuilder.newBuilder()
+                .build();
+        target = client.target(UriBuilder.fromPath(finalPATH).queryParam("fields","capital"));
     }
 
     public static CapitalCityRequest getInstance(String countryInput) {
@@ -11,7 +23,15 @@ public class CapitalCityRequest {
     }
 
     public CapitalCityResponse getCapitalCity() {
-        return null;
+        Response response = null;
+        try {
+            response = target.request().get();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        CapitalCityResponse capitalCity = CapitalCityResponse.getInstance(response).getCountries();
+        return capitalCity;
     }
 }
 
--- src/main/java/coding/challenge/rest/CapitalCityResponse.java
+++ src/main/java/coding/challenge/rest/CapitalCityResponse.java
@@ -1,4 +1,19 @@
 package coding.challenge.rest;
 
+import javax.ws.rs.core.Response;
+
 public class CapitalCityResponse {
+    private final String body;
+
+    private CapitalCityResponse(Response response){
+        body = response != null ? response.readEntity(String.class) : null;
+    }
+
+    public static CapitalCityResponse getInstance(Response response) {
+        return new CapitalCityResponse(response);
+    }
+
+    public CapitalCityResponse getCountries() {
+        return null;
+    }
 }
